dad98ee5ffd461edec0a959851170e81c708e3b1
Lalit <lsahu0838@gmail.com>
2023-02-28 18:52:59 +0530

    tool-tip

--- src/Components/ToolTip/ToolTip.jsx
+++ src/Components/ToolTip/ToolTip.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react'
 import { FaUserAlt } from 'react-icons/fa';
 import { MdOutlineAlternateEmail } from 'react-icons/md';
@@ -6,69 +7,37 @@ import './ToolTip.css'
 
 const ToolTip = () => {
     const [show, setShow] = useState()
-    const info = [
-        {
-            heading: 'Full Name',
-            icon: <FaUserAlt size={25} />,
-            toolTip: 'Email addres'
-        },
-        {
-            heading: 'Email',
-            icon: <MdOutlineAlternateEmail size={25} />,
-            toolTip: 'Name'
-        },
-        {
-            heading: 'Phone Number',
-            icon: <BsFillPhoneFill size={25} />,
-            toolTip: 'Phone Number'
-        },
+    const info = [{icon: <FaUserAlt size={25} />,toolTip: 'Full Name'},
+    {icon: <MdOutlineAlternateEmail size={25} />,toolTip: 'Email'},
+    {icon: <BsFillPhoneFill size={25} />,toolTip: 'Phone Number'},
 
     ]
     return (
         <ul className='toolItems'>
-            {/* <li className='Toolitem'>
-                <FaUserAlt size={25}/>
-                <div className="details">
-                    <h5>Full Name</h5>
-                    <p>Respondent's first and last name</p>
-                </div>
-            </li> */}
-
             {
                 info.map((inf, index) => {
-                    return (<li className='Toolitem' onMouseOver={()=>setShow(index)} onMouseLeave={()=>setShow('')}>
+                    return (<li className='Toolitem' 
+                    onMouseOver={() => setShow(index)} 
+                    onMouseLeave={() => setShow('')}>
                         {inf.icon}
                         <div className="details">
                             <h5>{inf.heading}</h5>
-                            <p>Respondent's first and last name</p>
-                            {
-                                show === index ? <div className="toolTip">
-                                    
-                                {/* <div className="tool"> */}
-                                    {inf.toolTip}
-                                {/* </div> */}
 
-                            </div> : null
-                            }
-                            
                         </div>
+                        {
+                            show === index ? <div className="toolTip">
+                                {inf.toolTip}
+                            </div> : null
+                        }
                     </li>)
                 })
             }
-            {/* <li className='Toolitem'>
-                <MdOutlineAlternateEmail size={25}/>
-                <div className="details">
-                    <h5>Full Name</h5>
-                    <p>Respondent's first and last name</p>
-                </div></li>
-            <li className='Toolitem'>
-                <BsFillPhoneFill size={25}/>
-                <div className="details">
-                    <h5>Full Name</h5>
-                    <p>Respondent's first and last name</p>
-                </div></li> */}
         </ul>
     )
 }
 
 export default ToolTip
+
+
+
+
